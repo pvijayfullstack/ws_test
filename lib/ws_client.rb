44600298bc1717ddd168462067b0c8879539fe21
Allan Davis <cajun.code@gmail.com>
2011-06-16 14:58:22 -0400

    fixed proxy issue

--- lib/ws_client.rb
+++ lib/ws_client.rb
@@ -4,9 +4,11 @@
   
   def build_proxy_url
       user_name = "nbsyuyg"
-      host = "171.156.66.181"
-      port = "8080"
       password = "br549nbs"
+      #host = "171.156.66.181"
+      host = "127.0.0.1"
+      port = "8080"
+      
       protocol = "http"
       # http://PROXY_USERNAME:PROXY_PASSW...@proxy.server.com:80
       if host.blank? or protocol.blank?
@@ -18,14 +20,24 @@
       proxy += ":#{password}" unless password.blank?
       proxy += "@" unless user_name.blank?
       proxy += "#{host}"
-      proxy += ":#{port}"unless port.blank?
+      proxy += ":#{port}/"unless port.blank?
       proxy
       
   end
-  def call
+  def service_call
+    
+    puts "Calling Web Services"
+    service_url = "https://dev-hlsc.bankofamerica.com/HealthLogicServices/services/SampleWebService"
+    proxy_url = build_proxy_url
+    opts = {}
+    #opts["protocol.http.basic_auth"] = [service_url, user_name, password]
+    opts['protocol.http.proxy'] = proxy_url
+    puts "Service URL\n\t#{service_url}"
+    puts "Proxy URL:\n\t#{proxy_url}"
+    puts "\n\n"
     soap_client = ActionWebService::Client::Soap.new(GreetAPI,
-                  "https://dev-hlsc.bankofamerica.com/HealthLogicServices/services/SampleWebService",
-                  :proxy => build_proxy_url)
+                  service_url, :driver_options => opts)
     puts soap_client.sayGreetings " Allan"
   end
   
+  service_call
